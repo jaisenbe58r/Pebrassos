8c88c206559cbe7b72a2404580f7ed185dc5253f
Jaime Sendra <jsendra@autis.es>
2020-10-05 23:47:16 +0200

    production debug

--- config/prod.py
+++ config/prod.py
@@ -16,6 +16,7 @@ from .default import *
 
 
 SECRET_KEY = '5e04a4955d8878191923e86fe6a0dfb24edb226c87d6c7787f35ba4698afc86e95cae409aebd47f7'
+DEBUG = True  # Quitar
 
 APP_ENV = APP_ENV_PRODUCTION
 
